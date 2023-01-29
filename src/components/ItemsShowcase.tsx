fbe9c5ea1a16e17ca25a362091fe21dd35fce26f
Karlos Ye <58801593+karlosye@users.noreply.github.com>
2023-01-28 20:04:35 -0500

    Added products section

--- src/components/ItemsShowcase.tsx
+++ src/components/ItemsShowcase.tsx
@@ -1,5 +1,7 @@
 import "./ItemsShowcase.css";
 
+import dummy_item_gallery from "../data/items-gallery.json";
+
 const ItemsShowcase = () => {
   return (
     <>
@@ -11,9 +13,25 @@ const ItemsShowcase = () => {
               <p className="product_text">Working on the product section</p>
             </div>
           </div>
-          <div className="row">
-            <div className="col-md-12">
-              <div className="owl-carousel owl-theme owl-loaded owl-drag"></div>
+          <div className="product-gallery">
+            <div className="row">
+              {dummy_item_gallery.map((item) => {
+                return (
+                  <div className="pic-container" key={item.id}>
+                    <div className="parent">
+                      <div className={`wrapper thumb-${item.id}`}>
+                        <div className="content">
+                          <div className="img">dsfd</div>
+                          <div className="text">
+                            <div className="line title"></div>
+                            <div className="line subtitle"></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
