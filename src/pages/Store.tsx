14c4e4955f354561c0eaff056bdef5161b024a23
Karlos Ye <58801593+karlosye@users.noreply.github.com>
2023-01-30 12:12:16 -0500

    COMMIT

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,14 +1,21 @@
 import { Col, Row } from "react-bootstrap";
 import storeItems from "../data/items.json";
 import StoreItem from "../components/StoreItem";
+import { useEffect } from "react";
 
 export const Store = () => {
+  window.scrollTo(0, 0);
+
   return (
     <>
       <h1>Store page</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
         {storeItems.map((item) => {
-          return <Col><StoreItem {...item}/></Col>;
+          return (
+            <Col>
+              <StoreItem {...item} />
+            </Col>
+          );
         })}
       </Row>
     </>
